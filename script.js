d98a5056c265c862499115d8f48db59c9608cf54
DevWanderson <guedesw3@gmail.com>
2024-05-21 15:55:09 -0300

    Revert "Atualização estrutura de arquivos"
    This reverts commit 5588fc38739f94cd98d152f4df7b8dd1b16aa431.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function toogleMode(){
     //pegar a tag img
     const image = document.querySelector("#profile img")
 
+
     if(html.classList.contains('light')){
     //se tiver light mode, adicionar a imagem light
         image.setAttribute('src', './assets/puppy.jpg')
@@ -18,4 +19,5 @@ function toogleMode(){
         //se tiver sem light mode, manter a imagem
         image.setAttribute('src', 'https://www.google.com/url?sa=i&url=http%3A%2F%2Ft0.gstatic.com%2Flicensed-image%3Fq%3Dtbn%3AANd9GcTZCSmCzmIPm0up8wmW566cK5w3sSTUChT5UnaU3VnFxrHwoRNSnks0xUBmj2r2oeJk&psig=AOvVaw2m6Gr5LD8TC6ebLNspqswI&ust=1711648990311000&source=images&cd=vfe&opi=89978449&ved=0CAoQjRxqFwoTCLD19MGDlYUDFQAAAAAdAAAAABAE')
     }
+
 }
